e2bdffcb5fd95374d92dfbbf02bcaa16ad542541
PedroBonorin0 <pepebonorinobraga@gmail.com>
2022-05-24 20:31:57 -0300

    Estrutura inicial do código

---
+++ coding_figth/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ coding_figth/src/pages/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <h1>HOME PAGE</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  components: {
+  },
+};
+</script>
