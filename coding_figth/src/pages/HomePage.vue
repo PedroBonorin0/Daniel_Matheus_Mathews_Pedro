179d656b2a7590db63730bc6b07481c2558b292c
Matheus dos Reis Casarim <matheuscasarim22@hotmail.com>
2022-05-31 10:57:07 -0300

    Teste na pagina Home

--- coding_figth/src/pages/HomePage.vue
+++ coding_figth/src/pages/HomePage.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="home">
     <h1>HOME PAGE</h1>
+    <Desafio />
+    
   </div>
 </template>
 
 <script>
+import Desafio from '../components/Questoes/Desafio.vue';
+
 export default {
   name: 'HomePage',
   components: {
-  },
+    Desafio
+}
 };
 </script>
