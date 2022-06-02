305cda0886a72862609f2ee35c467a0cdd73866c
PedroBonorin0 <pepebonorinobraga@gmail.com>
2022-06-01 21:58:15 -0300

    Sprite inimigos

--- coding_figth/src/pages/GamePage.vue
+++ coding_figth/src/pages/GamePage.vue
@@ -1,29 +1,44 @@
 <template>
-<div class="conteudo">
-  <div class="questao">
-    Questao: {{desafios[this.perguntaEscolhida].id}}
+<div class="geral">
+  <div class="personagens">
+    <SpritePersonagem
+      personagem="player"
+      :vivo="true"
+      :hp="playerHp"
+    />
+    <SpritePersonagem
+      personagem="enemy"
+      :vivo="true"
+      :hp="enemyHp"
+    />
   </div>
+  <div class="conteudo">
+    <div class="questao">
+      Questao: {{desafios[this.perguntaEscolhida].id}}
+    </div>
 
-  <div class="dificuldade">
-    <h1>Dificuldade: {{desafios[this.perguntaEscolhida].dificuldade}} </h1>
-  </div>
+    <div class="dificuldade">
+      <h1>Dificuldade: {{desafios[this.perguntaEscolhida].dificuldade}} </h1>
+    </div>
 
-  <div class="pergunta">
-    <h1>Pergunta: {{desafios[this.perguntaEscolhida].pergunta}} </h1>
-  </div>
+    <div class="pergunta">
+      <h1>Pergunta: {{desafios[this.perguntaEscolhida].pergunta}} </h1>
+    </div>
 
-  <div class="container-botoes">
-    <button
-      class="opcoes"
-      v-for="opcao in opcoesResposta"
-      :key="opcao.id"
-      @click="handleResposta(opcao)">{{opcao.resposta}}</button>
+    <div class="container-botoes">
+      <button
+        class="opcoes"
+        v-for="opcao in opcoesResposta"
+        :key="opcao.id"
+        @click="handleResposta(opcao)">{{opcao.resposta}}</button>
+    </div>
   </div>
 </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import SpritePersonagem from '@/components/SpritePersonagem.vue';
 
 export default {
   name: 'GamePage',
@@ -36,7 +51,13 @@ export default {
       opcoesResposta: '',
       respostaCorreta: '',
       dica: '',
+
+      playerHp: 100,
+      enemyHp: 100,
     };
+  },
+  components: {
+    SpritePersonagem,
   },
   created() {
     // Conta a qtd total de desafios de mesma dificuldade
@@ -245,5 +266,11 @@ export default {
 }
 .opcoes:hover {
   background-color: #281e5f;
+}
+
+.personagens {
+  width: 100%;
+  margin: 20px auto;
+  text-align: center;
 }
 </style>
