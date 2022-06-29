cdc8711cedb3aba5594f961c11bd5807573f32f5
Daniel Muller <danielmrezende@yahoo.com.br>
2022-06-29 20:05:44 -0300

    iniciando exibição de desafio

---
+++ coding_figth/src/components/ExibeDesafios.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="desafio-information">
+        <div class="desafio-info">{{ this.desafios }}</div>
+    </div>
+</template>
+
+<script>
+import { orderBy } from 'lodash-es';
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+    name: 'Desafio',
+    data(){
+        return{
+            perguntaDesafio: '',
+            opcao1: '',
+            opcao2: '',
+            opcao3: '',
+            opcao4: '',
+            dica: '',
+        };
+    },
+    mounted(){
+        this.setDesafio();
+        //this.
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
