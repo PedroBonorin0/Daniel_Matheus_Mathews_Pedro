1e37936b8ed7d7256a6d55ae3b6c67f6618afe7f
Matheus dos Reis Casarim <matheuscasarim22@hotmail.com>
2022-07-16 18:06:19 -0300

    Adição de documentação e alteração na função

--- Documentos/Teste_mecanica_Ranking.py
+++ Documentos/Teste_mecanica_Ranking.py
@@ -7,13 +7,28 @@ vida_player = 100
 vida_monster = 100
 dano_base = 20
 
+def calcula_pontuacao(vetor:list):
+      """ Calcula a pontuacao do player (Mecanica de ranking)
+      Arguments:
+            vetor: <List> Vetor que contem perguntas
+      Returns:
+            <Int> Pontuacao do player
+       """
+      pontuacao = 0
+      for i in vetor:
+            if(i.get('acerto')):
+                  pontuacao += i.get('dificuldade')
+      return int(pontuacao)
+
 def calcula_dano(dano_base:int, resposta:dict, player:int, monster:int):
       """ Calcula o dano que o player e o monster recebem 
-      dano_base: Constante que define o dano base
-      resposta: Dicionario que define se o player acertou ou nao
-      player: Vida do player
-      monster: Vida do monster
-      Retorna: (Vida do player, Vida do monster)
+      Arguments:
+            dano_base: <Int> Constante que define o dano base
+            resposta: <Dict> Dicionario que contem as informações das perguntas (se acertou, a dificuldade)
+            player: <Int> Vida do player
+            monster: <Int> Vida do monster
+      Returns:
+            <tuple[int, int]> (Vida do player, Vida do monster)
       """
       if(resposta.get('dificuldade') <= 3):##Facil
             if(resposta.get('acerto')):
@@ -37,7 +52,21 @@ def calcula_dano(dano_base:int, resposta:dict, player:int, monster:int):
 
       return player, monster
 
-def adicao_erros(erros, vida_player, vida_monster):
+def adicao_erros(erros:int, vida_player:int, vida_monster:int):
+      """ Adiciona erros no conjunto de respostas
+      Arguments:
+            erros: <Int> Numero de erros
+            vida_player: <Int> Vida do player
+            vida_monster: <Int> Vida do monster
+      Returns:
+            Dicionario com as respostas{
+                  'player' : <Int> vida do player, 
+                  'monster' : <Int> vidado monster, 
+                  'n' : <Int> numero da pergunta, 
+                  'dificuldade' : <Int> dificuldade da pergunta, 
+                  'acerto' : <Bolean> verificador de acerto
+                  }
+      """
       vetor = []
       player = vida_player
       monster = vida_monster
@@ -75,10 +104,18 @@ def adicao_erros(erros, vida_player, vida_monster):
       return auxiliar
 
 def qualificacao(vetor, verificador = False):
+      """ Adiciona qualificacao ao vetor de perguntas adicionando dificuldade 
+            ACERTO dificuldade + 1; 
+            ERRO dificuldade - 1;
+      Arguments:
+            vetor: <List> Vetor que contem perguntas
+            verificador: <Bolean> Verificador Para saber se o inicio do vetor já foi ranqueado
+      Returns:
+            <List> Vetor que contem as informações das perguntas com adição da dificuldade
+      """
       if(verificador):
             if(vetor[-1].get('acerto') == False):
-                  print("ERRO VETOR[-1] COMO ACERTO FALSE")
-                  return
+                  exit("ERRO VETOR[-1] COMO ACERTO FALSE")
             vetor.append({'acerto': True, 'dificuldade': vetor[-1].get('dificuldade') + 1})
       else:
             vetor[0]['dificuldade'] = 1
@@ -94,7 +131,15 @@ def qualificacao(vetor, verificador = False):
                         elif(vetor[i].get('dificuldade') + 1 == 11):
                               vetor[i+1]['dificuldade'] = 10
 
-def min_len_respostas(dano_base, player, monster):
+def min_len_respostas(dano_base:int, player:int, monster:int):
+      """ Retorna o numero minimo de respostas que o player e o monster devem responder
+      Arguments:
+            dano_base: <Int> Constante que define o dano base
+            player: <Int> Vida do player
+            monster: <Int> Vida do monster
+      Returns:
+            <List> vetor de respostas
+      """
       auxiliar = []
       vida_player = player
       vida_monster = monster
@@ -109,10 +154,11 @@ def min_len_respostas(dano_base, player, monster):
                   dificuldade = 10
       return auxiliar
 
-data = {'Player': [], 'Monster': [], 'nº pergunta': [], 'nº acerto': [], 'nº erro': [], 'nº erros esperados': [], 'Dsiposição': []}
+data = {'Player': [], 'Monster': [], 'nº pergunta': [], 'nº acerto': [], 'nº erro': [], 'nº erros esperados': [],'Pontuação': [], 'Dsiposição': []}
 Tamanho_teste = 100
 for index in range(Tamanho_teste):
       possibilidades_respostas_testes = []
+      possibilidades_respostas_testes.append(min_len_respostas(dano_base, vida_player, vida_monster))
       possibilidades_respostas_testes.append(adicao_erros(1, vida_player, vida_monster))
       possibilidades_respostas_testes.append(adicao_erros(2, vida_player, vida_monster))
       possibilidades_respostas_testes.append(adicao_erros(3, vida_player, vida_monster))
@@ -143,7 +189,8 @@ for index in range(Tamanho_teste):
             data.get('nº acerto').append(acerto)
             data.get('nº erro').append(erro)
             data.get('nº pergunta').append(len(possibilidades_respostas_testes[i]))
-            data.get('nº erros esperados').append(i + 1)
+            data.get('nº erros esperados').append(i)
+            data.get('Pontuação').append(calcula_pontuacao(possibilidades_respostas_testes[i]))
             for perguntas in possibilidades_respostas_testes[i]:
                   if(len(possibilidades_respostas_testes[i]) == len(disposicao_perguntas)):
                         disposicao_perguntas.append('FIM')
@@ -154,7 +201,5 @@ for index in range(Tamanho_teste):
             data.get('Dsiposição').append(disposicao_perguntas)
 
 tabela = pd.DataFrame(data)
-
-datetime.today().strftime('%H:%M:%S_%d-%m-%Y')
 tabela.to_excel('Documentos/'+str(datetime.today().strftime('%H_%M %d-%m')+'Dano'+str(dano_base)+'.xlsx'), index=False)
 # print(tabela)
